7aa5155dde19965a6c087798aaa9de3c10151f27
Abhay-Bhongade <abhaybhongade63@gmail.com>
2023-06-02 11:08:07 +0530

    commit message

--- src/Components/Navigation3/Navigation3.js
+++ src/Components/Navigation3/Navigation3.js
@@ -1029,7 +1029,7 @@ const Navigation3 = () => {
         <div className="col-md-9 p-0 m-0">
           <div className="imgcntinNavigationPage">
             <img
-              className="card-img-top img-fluid"
+              className="card-img-top img-fluid bnimg"
               src={homepage70000}
               alt="Card image cap"
             />
